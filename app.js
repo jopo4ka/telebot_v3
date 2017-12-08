e052fec58e4a29268b104851cee836fe0363e5c5
jopo4ka <sobakasobaka6@gmail.com>
2017-12-08 09:02:45 +0500

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var dbUtils = require('./modules/database/utils')
+var bot = require('./modules/bot/bot');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
